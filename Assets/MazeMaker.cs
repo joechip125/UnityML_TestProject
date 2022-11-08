c5fcfe778088f96b95c9ffd7e5d8f1e38f9964b1
joechip125 <mattias.lonnqvist@student.forsbergsskola.se>
2022-11-08 20:36:52 +0100

    started working on model based on Pacman

---
+++ Assets/MazeMaker.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class MazeMaker : MonoBehaviour
+{
+    public GameObject floorTile;
+    public int numberTiles;
+    
+    
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        for (int i = 0; i < numberTiles; i++)
+        {
+            
+        }
+    }
+}
