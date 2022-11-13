c4dc1025d5cb8ce4729622efe85a590880278883
joechip125 <mattias.lonnqvist@student.forsbergsskola.se>
2022-11-13 20:22:20 +0100

    update

--- Assets/CollectSpawner.cs
+++ Assets/CollectSpawner.cs
@@ -12,22 +12,51 @@ public class CollectSpawner : MonoBehaviour
     private List<GameObject> _spawnedObjeccts = new List<GameObject>(); 
     public int spawnAmount;
     public Transform owner;
+    public List<Transform> Locations = new List<Transform>();
+    public Vector3 spawnLoc;
 
-    public void Respawn(float rangeX, float rangeZ)
+    public void Init()
+    {
+        GetNewLocation();
+    }
+    public void SpawnSingle(int hitCount, int hitInterval)
+    {
+        DestroyAll();
+        if (hitCount % hitInterval == 0)
+        {
+         //   GetNewLocation();
+        }
+        _spawnedObjeccts.Add(Instantiate(spawnPrototype, Locations[hitCount].localPosition, Quaternion.identity, owner));
+    }
+
+    public void DestroyAll()
     {
         foreach (var o in _spawnedObjeccts)
         {
             if(o) Destroy(o);
         }
         _spawnedObjeccts.Clear();
+    }
+
+    public void Respawn(float rangeX, float rangeZ)
+    {
+        DestroyAll();
 
         for (int i = 0; i < spawnAmount; i++)
         {
-            var local = transform.localPosition + new Vector3(Random.Range(-rangeX, rangeX), 
+            var local = owner.localPosition + new Vector3(Random.Range(-rangeX, rangeX), 
                 0.2f,Random.Range(-rangeZ, rangeZ));
            
             _spawnedObjeccts.Add(Instantiate(spawnPrototype, local, Quaternion.identity, owner));
         }
+    }
+
+    public Vector3 GetNewLocation()
+    {
+        var vec = Locations[0].localPosition;
+        spawnLoc = vec;
+        Locations.RemoveAt(0);
+        return vec;
     }
 
     public void DestroyThis(GameObject toDestroy)
--- Assets/Scripts/HunterGather.cs
+++ Assets/Scripts/HunterGather.cs
@@ -19,14 +19,21 @@ public class HunterGather : Agent
     public Transform dropOff;
     private RayPerceptionSensorComponent3D _perception;
     public CollectSpawner spawner;
+    public int HitCount;
+    public float totalRotation;
+    public float rotateDiff;
+    public float lastRot;
+    private Vector3 lastForward;
     
     public override void OnEpisodeBegin()
     {
         transform.localPosition = dropOff.localPosition;
-        Debug.Log(dropOff.localPosition);
-        Debug.Log(transform.localPosition);
-        spawner.Respawn(6, 6);
+        transform.localRotation = Quaternion.identity;
+        lastRot = transform.localRotation.y;
+        if (HitCount > 4) HitCount = 0;
+        spawner.SpawnSingle(HitCount, 10);
         _haveCollect = false;
+        totalRotation = 0;
     }
 
     private void Start()
@@ -40,18 +47,26 @@ public class HunterGather : Agent
     {
         if (collision.gameObject.CompareTag("Wall"))
         {
-            SetReward(-1f);
-            EndEpisode();
+            AddReward(-0.02f);
         }
         if (collision.gameObject.CompareTag("Collectable"))
         {
             Destroy(collision.gameObject);
             SetReward(1f);
+            HitCount++;
             _haveCollect = true;
             EndEpisode();
         }   
     }
-    
+
+    private void OnCollisionStay(Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.CompareTag("Wall"))
+        {
+            AddReward(-0.02f);
+        }
+    }
+
 
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -60,38 +75,61 @@ public class HunterGather : Agent
         sensor.AddObservation(localVelocity.z);
     }
 
-    private float moveSpeed = 0.4f;
-    private float turnSpeed = 0.4f;
+    private float moveSpeed = 0.3f;
+    private float turnSpeed = 0.2f;
     public override void OnActionReceived(ActionBuffers actions)
     {
         AddReward(-0.005f);
-        
-        var forward = Mathf.Clamp(actions.ContinuousActions[0], -1f, 1f);
+        var dot = Vector3.Dot(lastForward, transform.forward);
+
+        var forward = Mathf.Clamp(actions.ContinuousActions[0], 0, 1f);
         var right = Mathf.Clamp(actions.ContinuousActions[1], -1f, 1f);
         var rotate = Mathf.Clamp(actions.ContinuousActions[2], -1f, 1f);
-        
+
         var dirToGo = transform.forward * forward;
         dirToGo += transform.right * right;
         var rotateDir = -transform.up * rotate;
-        
-        
+
+
         _rBody.AddForce(dirToGo * moveSpeed, ForceMode.VelocityChange);
-        transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed); 
-       
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed);
+
+        rotateDiff =Mathf.DeltaAngle(transform.rotation.eulerAngles.y, lastRot);
+
+        if (Math.Abs(rotateDiff) > 0.5f)
+        {
+            AddReward(-0.05f);
+            Debug.Log(dot);
+        }
+        if (rotateDir.y != 0)
+        {
+            totalRotation += Time.fixedDeltaTime * turnSpeed;
+            //Debug.Log(totalRotation);
+            //Debug.Log((transform.rotation.eulerAngles));
+        }
+        
+        if (totalRotation > 400)
+        {
+            AddReward(-0.005f);
+        }
+        
         if (transform.localPosition.y < -1)
         {
             SetReward(-1f);
             EndEpisode();
         }
 
-       // if (_haveCollect)
-       // {
-       //     var dist = Vector3.Distance(transform.localPosition, dropOff.localPosition);
-       //     if (dist < 0.5f)
-       //     {
-       //         SetReward(1f);
-       //         EndEpisode();
-       //     }
-       // }
+        lastRot = transform.rotation.eulerAngles.y;
+        lastForward = transform.forward;
+
+        // if (_haveCollect)
+        // {
+        //     var dist = Vector3.Distance(transform.localPosition, dropOff.localPosition);
+        //     if (dist < 0.5f)
+        //     {
+        //         SetReward(1f);
+        //         EndEpisode();
+        //     }
+        // }
     }
 }
