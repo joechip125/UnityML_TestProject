f6f9512b4f2971314559d5d9c88a06777531dd99
Mattias Lönnqvist <joechip125@gmail.com>
2022-12-23 00:21:25 +0100

    update

--- Assets/Scripts/Trace/TraceLet.cs
+++ Assets/Scripts/Trace/TraceLet.cs
@@ -1,11 +1,43 @@
 ﻿using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Trace
 {
     [Serializable]
-    public class TraceLet
+    public class TraceLet : MonoBehaviour
     {
         public Vector3 mainDirection;
+        public float maxRange = 12;
+        private bool _moveToTarget = false;
+        private Vector3 _target;
+        private Vector3 _aPos;
+        private float _time = 0;
+        private float _distance;
+        private float _speed = 1;
+
+        private void FixedUpdate()
+        {
+            _distance = Vector3.Distance(transform.position, transform.parent.position);
+            if (_distance > maxRange && !_moveToTarget)
+            {
+                _time = 0;
+                _target = transform.parent.position;
+                _aPos = transform.position;
+                _moveToTarget = true;
+            }
+
+            if (_moveToTarget)
+            {
+                Vector3.LerpUnclamped(_aPos, _target, _time);
+                _time += (1 / _distance) * (Time.deltaTime * _speed);
+
+                if (_distance < maxRange)
+                {
+                    _moveToTarget = false;
+                    _time = 0;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Trace/TraceTest.cs
+++ Assets/Scripts/Trace/TraceTest.cs
@@ -12,6 +12,7 @@ public class TraceTest : MonoBehaviour
     [SerializeField] private float placeSphereRadius = 1.5f;
     private int _currentDots;
     private bool _flip;
+    private Vector3 _currentPos;
 
     void Start()
     {
@@ -21,14 +22,28 @@ public class TraceTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= new Vector3(0, 0, 0.5f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(0, 0, 0.5f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0.5f * Time.deltaTime,0,0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0.5f * Time.deltaTime,0,0);
+        }
     }
 
     private void ChangeDots()
     {
         if (numberDots > _currentDots)
         {
-            var add = numberDots - _currentDots;
             var center = transform.position;
             var aPos = center + placePos;
             
@@ -37,11 +52,6 @@ public class TraceTest : MonoBehaviour
                 var addDir = (transform.right + new Vector3(0,0,numberDots * 0.3f)) * 12;
                 var addPos = aPos + addDir;
                 traceLocations.Add(addPos);
-            }
-
-            foreach (var t in traceLocations)
-            {
-                
             }
         }
         
@@ -56,22 +66,50 @@ public class TraceTest : MonoBehaviour
         _currentDots = numberDots;
     }
 
+    private void MoveDots()
+    {
+        foreach (var t in traceLocations)
+        {
+            
+        }
+    }
+
+    private Vector3 GetDirectionFromRotation(float yRot)
+    {
+        return (transform.rotation 
+                * Quaternion.Euler(0, yRot, 0))
+                * Vector3.forward;
+    }
+
     private void OnDrawGizmos()
     {
         if (_currentDots != numberDots)
         {
             ChangeDots();
         }
-        
+
+
         var center = transform.position;
+        Gizmos.DrawRay(center, GetDirectionFromRotation(34));
+        Gizmos.DrawRay(center, GetDirectionFromRotation(180));
+        
+        if (Vector3.Distance(_currentPos, center) > 0.1f)
+        {
+            MoveDots();
+            
+        }
         Gizmos.color = Color.magenta;
         Gizmos.DrawWireSphere(center, 3.0f);
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube( center + placePos, new Vector3(4,4, 4 * numberDots));
 
-        foreach (var t in traceLocations)
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(center, t);
-            Gizmos.DrawWireSphere(t, placeSphereRadius);
-        }
+
+        //foreach (var t in traceLocations)
+        //{
+        //    Gizmos.color = Color.green;
+        //    Gizmos.DrawLine(center, t);
+        //    Gizmos.DrawWireSphere(t, placeSphereRadius);
+        //}
     }
 }
